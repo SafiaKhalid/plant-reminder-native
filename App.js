94110a97d1fa545859eeb4cfec8cff18740eb767
SafiaKhalid <safia.am.khalid@gmail.com>
2024-07-30 16:14:40 +0100

    Added plant card data t0 Home screen

--- App.js
+++ App.js
@@ -11,22 +11,11 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ headerShown: false }}
-        />
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,7 +1,34 @@
 import React from "react";
-import { View, Text } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
 
 const Home = () => {
+    const data = [
+        {
+            plantId:'1',
+            image:'https://images.pexels.com/photos/1974508/pexels-photo-1974508.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+            name:'Basil',
+            species:'Ocimum basilicum',
+            waterInterval:'20',
+            timeLeft: '13' ,
+            age: {
+            years: '2',
+            months: '3'
+            }
+        },
+        {
+            plantId:'2',
+            image:'https://images.pexels.com/photos/1974508/pexels-photo-1974508.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+            name:'Mint',
+            species:'Mentha piperita',
+            waterInterval:'3',
+            timeLeft: '1' ,
+            age: {
+                years: '1',
+                months: '4'
+            }
+        }
+    ]
+
     return <View>
         <Text>Home</Text>
     </View>
