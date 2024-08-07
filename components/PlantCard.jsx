5c82dca6241e17f35bc6dabceaaa22d5ce074757
SafiaKhalid <safia.am.khalid@gmail.com>
2024-08-07 14:45:29 +0100

    Imported packages

--- components/PlantCard.jsx
+++ components/PlantCard.jsx
@@ -85,7 +85,6 @@ export default PlantCard
 //Water button
 
 
-//Branch - delete plant card (are you sure modal, add to edit plant screen)
 //Branch - add image to plant card
 //Branch - order plant card list (time left to water?)
 //Branch - notifications
